85a1e8e64881d13076e1920ce92e60bf6bb93f24
applepinepaprica <applepinepaprica@gmail.com>
2019-04-06 10:53:42 +0400

    Added query for single objects

--- myproject/link_shortener/schema.py
+++ myproject/link_shortener/schema.py
@@ -11,7 +11,28 @@ class LinkType(DjangoObjectType):
 
 
 class Query(object):
+    link = graphene.Field(LinkType,
+                              id=graphene.Int(),
+                              shortLink=graphene.String(),
+                              fullLink=graphene.String())
+
     all_links = graphene.List(LinkType)
 
     def resolve_all_links(self, info, **kwargs):
         return Link.objects.all()
+
+    def resolve_link(self, info, **kwargs):
+        id = kwargs.get('id')
+        shortLink = kwargs.get('shortLink')
+        fullLink = kwargs.get('fullLink')
+
+        if id is not None:
+            return Link.objects.get(pk=id)
+
+        if shortLink is not None:
+            return Link.objects.get(short_link=shortLink)
+
+        if fullLink is not None:
+            return Link.objects.get(full_link=fullLink)
+
+        return None
